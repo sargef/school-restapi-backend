180161cf2135330a35321351b311f7c67fb5a4c1
Sarah Shelley <35126840+sargef@users.noreply.github.com>
2020-04-19 15:41:24 +0930

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,36 +1,79 @@
 'use strict';
-const Sequelize = require('sequelize');
+const bcryptjs = require('bcryptjs');
 
-module.exports = (sequelize) => {
-  class User extends Sequelize.Model {}
-  User.init({
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
+     allowNull: false,
+     autoIncrement: true,
+     primaryKey: true,
+     type: DataTypes.INTEGER
+   },
     firstName: {
-      type: Sequelize.STRING,
-    },
+     type: DataTypes.STRING,
+     allowNull: false,
+     validate: {
+       notNull: {
+         msg: 'Please provide a first name!'
+       },
+       notEmpty: {
+         msg: 'Please provide a first name!'
+       },
+     },
+   },
     lastName: {
-      type: Sequelize.STRING,
-    },
-    emailAddress: {
-        type: Sequelize.STRING,
+     type: DataTypes.STRING,
+     allowNull: false,
+     validate: {
+      notNull: {
+        msg: 'Please provide a last name!',
       },
+      notEmpty: {
+        msg: 'Please provide a last name!',
+      },
+     },
+   },
+    emailAddress: {
+     type: DataTypes.STRING,
+     allowNull: false,
+     unique: {args: true, msg: 'This email address is already taken!'},
+     validate: {
+      notNull: {
+        msg: 'Please provide an email address!',
+      },
+      notEmpty: {
+        msg: 'Please provide an email address!',
+      },
+      isEmail: {
+        msg: 'Please enter a valid email address!',
+      },
+     },
+   },
     password: {
-        type: Sequelize.STRING,
-      }
-  }, { sequelize });
-
-  User.associate = (models) => {
-    User.hasMany(models.Course, { 
-      foreignKey: {
-        fieldName: 'userId', 
-        allowNull: false,
-          },
-      });
-  };
-
+     type: DataTypes.STRING,
+     allowNull: false,
+     set(value) {
+       if (value !== '' && value !== null) {
+         this.setDataValue('password', bcryptjs.hashSync(value))
+       }
+     },
+     validate: {
+      notNull: {
+        msg: 'Please provide a password!',
+      },
+      notEmpty: {
+        msg: 'Please provide a password!',
+      },
+     },
+   }
+  }, {});
+  User.associate = function(models) {
+    User.hasMany(models.Course, {
+   foreignKey: {
+     fieldName: 'userId',
+     allowNull: false,
+   },
+ });
+};
   return User;
 };
